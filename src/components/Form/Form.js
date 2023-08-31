9df8f4463fc51149ef2185d17c2254f456e0c921
DEX <golden.rodjer123@gmail.com>
2023-08-31 20:03:52 +0600

    fix

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,4 +1,4 @@
-import Input from "../input/Input";
+import Input from "../Input/Input";
 import Button from "../button/Button";
 
 function Form({ placeholder, value, onClick, inputValue, setInputValue }) {
